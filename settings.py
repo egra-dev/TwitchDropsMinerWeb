4d6434c961f739c19e39acc230425d9999ed8d73
Egra-dev <106100526+egra-dev@users.noreply.github.com>
2026-09-01 15:31:10 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,26 +16,33 @@ class StateFile(TypedDict):
     restartRequest: bool
 
 class State:
+    PASSTHROUGH = ("_settings", "_args")
+
     def __init__(self):
-        self._statusSettings: StateFile = json_load(STATE_PATH, default_state)
+        self._settings: StateFile = json_load(STATE_PATH, default_state)
 
     def save(self) -> None:
         with open(STATE_PATH, 'w') as f:
-            json.dump(self._statusSettings, f, default=_serialize)
+            json.dump(self._settings, f, default=_serialize)
 
-    # default logic of reading settings is to check args first, then the settings file
     def __getattr__(self, name: str, /) -> Any:
-        if hasattr(self._args, name):
-            return getattr(self._args, name)
-        elif name in self._statusSettings:
-            return self._statusSettings[name]  # type: ignore[literal-required]
-        return getattr(super(), name)
-
+            if name in self.PASSTHROUGH:
+                # passthrough
+                return getattr(super(), name)
+            elif hasattr(self._args, name):
+                return getattr(self._args, name)
+            elif name in self._settings:
+                return self._settings[name]  # type: ignore[literal-required]
+            return getattr(super(), name)
+    
     def __setattr__(self, name: str, value: Any, /) -> None:
-        if name in self._statusSettings:
-            self._statusSettings[name] = value  # type: ignore[literal-required]
+        if name in self.PASSTHROUGH:
+            # passthrough
+            return super().__setattr__(name, value)
+        elif name in self._settings:
+            self._settings[name] = value  # type: ignore[literal-required]
+            self._altered = True
             return
-        
         raise TypeError(f"{name} is missing a custom setter")
 
 class SettingsFile(TypedDict):
