e7349a28721edda779f4152a595f736c20e45294
Egra-dev <106100526+egra-dev@users.noreply.github.com>
2026-09-06 14:08:54 +0300

    Update lazy-loader.js

--- web/static/js/lazy-loader.js
+++ web/static/js/lazy-loader.js
@@ -181,7 +181,7 @@ function createCampaignCard(campaign) {
         <div class="flex flex-col w-full">
             <div class="flex">
                 <!-- Left side with campaign info -->
-                <div class="w-1/4 flex flex-col items-center pr-3">
+                <div class="w-1/6 flex flex-col items-center pr-3">
                     ${campaign.image_url ? 
                     `<div class="mb-2">
                         <img data-src="${campaign.image_url}" alt="${campaign.name}" class="w-24 h-24 object-cover rounded lazy-image" src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1 1'%3E%3C/svg%3E">
